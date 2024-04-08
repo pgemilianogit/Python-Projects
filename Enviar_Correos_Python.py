84f060d149073d9db52449a79302af168fd5623a
Emiliano <panfu.100@hotmail.com>
2024-04-08 14:18:08 -0600

    Cambios con archivo adjunto

--- Enviar_Correos_Python.py
+++ Enviar_Correos_Python.py
@@ -1,6 +1,9 @@
 import smtplib                                      #LIBERIA DE ENVIOS
 from email.mime.text import MIMEText                #Permite ajustar texto plano o archivos html
 from email.mime.multipart import MIMEMultipart  
+from email.mime.base import MIMEBase
+from email import encoders                          #Enviar archivo adjunto
+import requests                                     # Importar la biblioteca requests
 
 #Generar contraseñas de aplicaciones
 
@@ -19,11 +22,30 @@ mensaje['To'] = destinatario
 mensaje['Subject'] = asunto
 
 #CUERPO DEL MENSAJE
-cuerpo = "Hola pruebas de enviar correo por python"
+cuerpo = "Hola, pruebas de enviar correo por Python con una imagen adjunta."
 #Adjuntar partes del correo, adjuntar componentes del correo y el contructor crea una instancia que representa el mesaje
 #varieble que contiene el desarrollo, indicando que el tipo de texto es plano, sin formato especial, negita, cursiva, etc.
 mensaje.attach(MIMEText( cuerpo, "plain" ))
 
+# DESCARGAR ARCHIVO DESDE URL
+url_de_la_imagen = 'https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg'
+nombre_del_archivo_local = 'archivo_descargado.pdf'  # Este será el nombre del archivo localmente
+
+# Realizar la petición GET al URL
+respuesta = requests.get('https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg')
+
+# Guardar el contenido descargado en un archivo en el sistema local
+with open(nombre_del_archivo_local, 'wb') as archivo_local:
+    archivo_local.write(respuesta.content)
+
+# AGREGAR ARCHIVO ADJUNTO LOCAL
+archivo_adjunto_path = "ruta/al/archivo.pdf"  # Asegúrate de cambiar esto por la ruta real del archivo
+parte = MIMEBase('application', "octet-stream")
+with open(archivo_adjunto_path, 'rb') as archivo:
+    parte.set_payload(archivo.read())
+encoders.encode_base64(parte)
+parte.add_header('Content-Disposition', f'attachment; filename={archivo_adjunto_path}')
+mensaje.attach(parte)
 
 #INICIAR SESIÓN EN SERVIDOR SMTP DE GMAIL
 server = smtplib.SMTP("smtp.gmail.com",587)         #Libreria con la direccion y el puerto
